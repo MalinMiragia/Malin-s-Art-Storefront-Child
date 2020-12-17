fa9157467a42fcf3b3c425387f320f00e2752e43
MalinMiragia <Malin.Olsson@medieinstitutet.se>
2020-12-17 11:28:24 +0100

    Some added templates, and changes

--- front-page.php
+++ front-page.php
@@ -4,38 +4,44 @@ defined( 'ABSPATH' ) || exit;
 
 get_header(); 
 ?>
+
 <div class="container">
 
 	<div class="wrapper" id="frontpage-wrapper">
-
+  <h1>Vad letar du efter idag?</h1>
 		<div class="row">
 
-			<div class="col-md-6 categories">
+			
 				<?
 				$catTerms = get_terms('product_cat', array('hide_empty' => 0, 'orderby' => 'ASC'));
 				foreach($catTerms as $catTerm) : ?>
-
+        <div class="col-md-6 categories">
+          <div class="categorie-img">
 				<?php $thumbnail_id = get_woocommerce_term_meta( $catTerm->term_id, 'thumbnail_id', true ); 
 					$image = wp_get_attachment_url( $thumbnail_id );  ?>
+          
+           <a href="<?php echo "http://localhost/Woocomerce Projekt/wordpress/index.php/produkt-kategori/" . $catTerm->slug ;?>">
+          <!-- Borde skrivas så här -->
+           <!-- get_term_link($catTerm)  -->
 
-					<a href="<?php echo site_url();?>/produkt-kategori/index.php/<?php echo $catTerm->slug; ?>">
-
-						<img src="<?php echo $image; ?>"></a>
-
-	 				<a href="<?php echo site_url();?>/produkt-kategori/index.php/<?php echo $catTerm->slug; ?>">  <h2><img src="<?php echo $a['url']; ?>"> 
-
-	   			<?php echo $catTerm->name; ?></h2></a>
-
+            <img src="<? echo $image; ?>"></a>
+             
+            
+	 				<div class="centered"><a href="<?php get_term_link($catTerm);?><?php echo $catTerm->slug; ?>">  <h2><img src="<?php echo $a['url']; ?>"> 
+           <?php echo $catTerm->name; ?></h2></a>
+           </div>
+          </div>
+           </div>
 				<?php endforeach; ?>
 
-			</div>
+			
 
 		</div>
 
+<h1> Nyaste tillagda produkter </h1>
+	<div class="row"> 
 
-	<!-- <div class="row"> -->
-
-		<div class="col-md-3 products">
+		
 
 <?
 	if(!function_exists('wc_get_products')) {
@@ -64,23 +70,33 @@ get_header();
   wc_set_loop_prop('total', $products_ids->total);
   wc_set_loop_prop('total_pages', $products_ids->max_num_pages);
 
+  
+
   if($products_ids) {
-    do_action('woocommerce_before_shop_loop');
-    woocommerce_product_loop_start();
+    //do_action('woocommerce_before_shop_loop');
+    //woocommerce_product_loop_start();
       foreach($products_ids->products as $featured_product) {
+        ?>
+        <div class="col-md-3 products">
+          <?
         $post_object = get_post($featured_product);
         setup_postdata($GLOBALS['post'] =& $post_object);
         wc_get_template_part('content', 'product');
+        ?>
+      </div>
+      <?
       }
       wp_reset_postdata();
-    woocommerce_product_loop_end();
-    do_action('woocommerce_after_shop_loop');
+      
+    //woocommerce_product_loop_end();
+
+   // do_action('woocommerce_after_shop_loop');
   } else {
     do_action('woocommerce_no_products_found');
   }
  
 ?>
-			</div>
+			
 		</div>
 	</div>
 </div>
--- functions.php
+++ functions.php
@@ -28,18 +28,18 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_meta', 30);
 
 
-//Change the number of products per page
-add_filter( 'loop_shop_per_page', 'malinsart_products_per_page',20);
-function malinsart_products_per_page( $num_products )   {
-    return 20;
-}
+// //Change the number of products per page
+// add_filter( 'loop_shop_per_page', 'malinsart_products_per_page',20);
+// function malinsart_products_per_page( $num_products )   {
+//     return 20;
+// }
 
-//Change the number of colums
+// //Change the number of colums
 
-add_filter( 'loop_shop_colums', 'malinsart_number_colums',20);
-function malinsart_number_colums( $cols )   {
-    return 5;
-}
+// add_filter( 'loop_shop_colums', 'malinsart_number_colums',20);
+// function malinsart_number_colums( $cols )   {
+//     return 5;
+// }
 
 // Changing the sales logo
 
